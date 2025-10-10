4aaffee1f54c48644ff61003513c078f544850c6
Jorge Clênio | @jrgcln <cleniocontato@gmail.com>
2025-10-10 10:23:08 -0300

    novas alterações
    alterações 10/10/2025 - 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { AboutComponent } from './pages/about/about.component';
 import { ExperienceComponent } from './pages/experience/experience.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ToggleThemeComponent } from './shared/components/toggle-theme/toggle-theme.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, AboutComponent, ExperienceComponent, ProjectsComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, AboutComponent, ExperienceComponent, ProjectsComponent, NotFoundComponent, ToggleThemeComponent],
   imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,1 +1,2 @@
 export { NavbarComponent } from './navbar/navbar.component';
+export { ToggleThemeComponent } from './toggle-theme/toggle-theme.component';
